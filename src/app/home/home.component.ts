af73fc278ddcccce9af4e1de69f55e552ec334bd
jdmartens <jeremydmartens@gmail.com>
2025-02-14 21:09:53 -0600

    rough in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { ExcuseService } from '../excuse.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  excuse: string = '';
 
+  constructor(private excuseService: ExcuseService) { }
+
+  getExcuse(type: string) {
+    switch (type) {
+      case 'work':
+        this.excuseService.getWorkExcuse().subscribe(excuse => this.excuse = excuse);
+        break;
+      case 'late':
+        this.excuseService.getLateExcuse().subscribe(excuse => this.excuse = excuse);
+        break;
+      case 'help':
+        this.excuseService.getHelpExcuse().subscribe(excuse => this.excuse = excuse);
+        break;
+      case 'busy':
+        this.excuseService.getBusyExcuse().subscribe(excuse => this.excuse = excuse);
+        break;
+    }
+  }
 }

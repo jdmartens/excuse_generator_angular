75709bf478d440afcfe65941471ee240d27b53ef
jdmartens <jeremydmartens@gmail.com>
2025-02-15 17:35:46 -0600

    define routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { AdvancedExcuseComponent } from './advanced-excuse/advanced-excuse.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'advanced-excuse', component: AdvancedExcuseComponent },
+  { path: 'about', component: AboutComponent },
+];

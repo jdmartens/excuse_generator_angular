208f92a4041f80aa9a15109026e10d0b50d05a9c
jdmartens <jeremydmartens@gmail.com>
2025-02-14 21:08:24 -0600

    backend service

--- src/app/excuse-api.service.ts
+++ src/app/excuse-api.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExcuseApiService {
+  private apiUrl = 'http://your-api-url.com';
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getWorkExcuse(): Observable<string> {
+    return this.http.get<string>(`${this.apiUrl}/work-excuse`);
+  }
+
+  getLateExcuse(): Observable<string> {
+    return this.http.get<string>(`${this.apiUrl}/late-excuse`);
+  }
+
+  getHelpExcuse(): Observable<string> {
+    return this.http.get<string>(`${this.apiUrl}/help-excuse`);
+  }
+
+  getBusyExcuse(): Observable<string> {
+    return this.http.get<string>(`${this.apiUrl}/busy-excuse`);
+  }
 }

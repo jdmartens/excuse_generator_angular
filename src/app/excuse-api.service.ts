1efce368189d01b0286ad70d54b6317cfb899d8b
jdmartens <jeremydmartens@gmail.com>
2025-02-16 16:52:27 -0600

    use apiUrl from env

--- src/app/excuse-api.service.ts
+++ src/app/excuse-api.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from './../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExcuseApiService {
-  private apiUrl = 'http://localhost:8000';
-  
+
   constructor(private http: HttpClient) { }
 
   getExcuse(type: 'work' | 'late' | 'help' | 'busy'): Observable<any> {
-    return this.http.get<string>(`${this.apiUrl}/${type}-excuse`);
+    return this.http.get<string>(`${environment.apiUrl}/${type}-excuse`);
   }
 }

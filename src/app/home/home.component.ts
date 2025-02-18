5768374667f03079620aafa177be12755ef8e7a5
jdmartens <jeremydmartens@gmail.com>
2025-02-17 21:24:39 -0600

    add wait spinner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ExcuseApiService } from '../excuse-api.service';
 
 @Component({
@@ -8,12 +9,33 @@ import { ExcuseApiService } from '../excuse-api.service';
 })
 export class HomeComponent {
   excuse: string = '';
+  loading: boolean = false;
 
-  constructor(private excuseService: ExcuseApiService) { }
+  constructor(private excuseService: ExcuseApiService, private snackBar: MatSnackBar) { }
 
   getExcuse(type: 'work' | 'late' | 'help' | 'busy') {
+    this.loading = true;
+    let snackBarRef;
+    const snackBarTimeout = setTimeout(() => {
+      snackBarRef = this.snackBar.open('The first request may take up to 45 seconds because it is hosted with minimal resources.', 'Close', {
+        duration: 30000, // 30 seconds
+      });
+    }, 10000); // 10 seconds
+
     this.excuseService.getExcuse(type).subscribe(excuse => {
-      this.excuse = excuse['excuse']
+      this.excuse = excuse['excuse'];
+      this.loading = false;
+      clearTimeout(snackBarTimeout);
+      if (snackBarRef) {
+        snackBarRef.dismiss();
+      }
+    }, error => {
+      this.loading = false;
+      clearTimeout(snackBarTimeout);
+      if (snackBarRef) {
+        snackBarRef.dismiss();
+      }
+      console.error('Error fetching excuse:', error);
     });
   }
 }
